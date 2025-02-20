2e24653051fb1f10ec290a5f38d2f484c35254ca
Ana Caroline <212008482@aluno.unb.br>
2025-02-20 16:51:23 -0300

    Test: Usando o Setup para C

--- blockly/generator.js
+++ blockly/generator.js
@@ -2,11 +2,12 @@
 //----------------------------------------------------------------------------------------------------------------
 // Generator
   
-Blockly.JavaScript['setup'] = function(block, generator) {
-  var statements_name = Blockly.JavaScript.statementToCode(block, 'Void_setup');
+Blockly.C['setup'] = function(block, generator) {
+  var statements_name = Blockly.C.statementToCode(block, 'Void_setup');
   // TODO: Assemble JavaScript into code variable.
-  var code = '<br><br><font color="#00979c">void</font> <font color="#5e6d03">setup</font><font color="#000000">(</font><font color="#000000">)</font><br><font color="#000000">{</font>&nbsp;'+statements_name+'<br><font color="#000000">}</font>';
-    return code;
+  //var code = '<br><br><font color="#00979c">void</font> <font color="#5e6d03">setup</font><font color="#000000">(</font><font color="#000000">)</font><br><font color="#000000">{</font>&nbsp;'+statements_name+'<br><font color="#000000">}</font>';
+  var code = "void setup() { " + statements_name + "}";
+  return code;
 };
 
 Blockly.JavaScript['loop'] = function(block, generator) {
