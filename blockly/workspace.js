e2101d0f6754703655291b540a681e10b636aad6
Ana Caroline <212008482@aluno.unb.br>
2025-02-21 20:54:51 -0300

    Fix error 2

--- blockly/workspace.js
+++ blockly/workspace.js
@@ -33,12 +33,12 @@ var options = {
 };
 
 /* Inject your workspace */ 
-var workspace = Blockly.inject(/* TODO: Add ID of div to inject Blockly into */ options);
+var workspace = Blockly.inject('blocklyDiv',options);
 
 /* Load Workspace Blocks from XML to workspace. Remove all code below if no blocks to load */
 
-/* TODO: Change workspace blocks XML ID if necessary. Can export workspace blocks XML from Workspace Factory. */
-var workspaceBlocks = document.getElementById("workspaceBlocks"); 
+// /* TODO: Change workspace blocks XML ID if necessary. Can export workspace blocks XML from Workspace Factory. */
+// var workspaceBlocks = document.getElementById("workspaceBlocks"); 
 
-/* Load blocks to workspace. */
-Blockly.Xml.domToWorkspace(workspaceBlocks, workspace);
+// /* Load blocks to workspace. */
+// Blockly.Xml.domToWorkspace(workspaceBlocks, workspace);
