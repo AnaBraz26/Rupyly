227041a0e3e4d29d3a67b96213682db5a2fe1dc5
Ana Caroline <212008482@aluno.unb.br>
2025-04-07 20:30:33 -0300

    Java: Testando conexão do java

--- blockly/generator.js
+++ blockly/generator.js
@@ -8,21 +8,24 @@ Blockly.C.name = 'C';
 
 Blockly.C['setup'] = function(block, generator) {
   var statements_name = Blockly.C.statementToCode(block, 'Void_setup');
-  //var code = '<br><br><font color="#00979c">void</font> <font color="#5e6d03">setup</font><font color="#000000">(</font><font color="#000000">)</font><br><font color="#000000">{</font>&nbsp;'+statements_name+'<br><font color="#000000">}</font>';
-  return "void setup(){\n" +statements_name+ "\n}";
+  var code = '<br><br><font color="#00979c">void</font> <font color="#5e6d03">setup</font><font color="#000000">(</font><font color="#000000">)</font><br><font color="#000000">{</font>&nbsp;'+statements_name+'<br><font color="#000000">}</font>';
+  // return "void setup(){\n" +statements_name+ "\n}";
+  return code;
 };
 
 Blockly.C['loop'] = function(block, generator) {
   var statements_name = Blockly.C.statementToCode(block, 'Void_loop');
-  //var code = '<br><br><font color="#00979c">void</font> <font color="#5e6d03">loop</font><font color="#000000">(</font><font color="#000000">)</font><br><font color="#000000">{</font>&nbsp;'+statements_name+'<br><font color="#000000">}</font>';
-    return "void loop(){\n" + statements_name +"\n}";
+  var code = '<br><br><font color="#00979c">void</font> <font color="#5e6d03">loop</font><font color="#000000">(</font><font color="#000000">)</font><br><font color="#000000">{</font>&nbsp;'+statements_name+'<br><font color="#000000">}</font>';
+    // return "void loop(){\n" + statements_name +"\n}";
+    return code;
 };
 
 Blockly.C['led'] = function(block, generator) {
   var text_nome = block.getFieldValue('nome');
   var number_porta = block.getFieldValue('Porta');
-  //var code = '<br><font color="#00979c">int </font><font color="#000000">led'+ text_nome +'</font><font color="#434f54"> =</font> <font color="#000000">'+number_porta+'</font><font color="#000000">;</font>';
-  return "int led"+text_nome+" = "+number_porta+";";
+  var code = '<br><font color="#00979c">int </font><font color="#000000">led'+ text_nome +'</font><font color="#434f54"> =</font> <font color="#000000">'+number_porta+'</font><font color="#000000">;</font>';
+  // return "int led"+text_nome+" = "+number_porta+";";
+  return code;
 };
 
 Blockly.C['pinmode_led'] = function(block, generator) {
