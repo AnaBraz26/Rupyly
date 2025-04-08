c8090ce142af0e24b01e294e95a670a494573a78
Ana Caroline <212008482@aluno.unb.br>
2025-04-07 21:05:09 -0300

    C6: Testando conexão do C

--- blockly/workspace.js
+++ blockly/workspace.js
@@ -1,39 +1,44 @@
-window.addEventListener("load", function () {
-	var toolbox = document.getElementById("toolbox");
+/* TODO: Change toolbox XML ID if necessary. Can export toolbox XML from Workspace Factory. */
+var toolbox = document.getElementById("toolbox");
 
-	var options = { 
-		toolbox : toolbox, 
-		collapse : true, 
-		comments : true, 
-		disable : false, 
-		maxBlocks : Infinity, 
-		trashcan : true, 
-		horizontalLayout : false, 
-		toolboxPosition : 'start', 
-		css : true, 
-		media : 'https://blockly-demo.appspot.com/static/media/', 
-		rtl : false, 
-		scrollbars : true, 
-		sounds : true, 
-		oneBasedIndex : true, 
-		grid : {
-			spacing : 100, 
-			length : 5, 
-			colour : '#888', 
-			snap : false
-		}, 
-		zoom : {
-			controls : true, 
-			wheel : true, 
-			startScale : 5, 
-			maxScale : 15, 
-			minScale : 0.75, 
-			scaleSpeed : 1.2
-		}
-	};
+var options = { 
+	toolbox : toolbox, 
+	collapse : true, 
+	comments : true, 
+	disable : false, 
+	maxBlocks : Infinity, 
+	trashcan : true, 
+	horizontalLayout : false, 
+	toolboxPosition : 'start', 
+	css : true, 
+	media : 'https://blockly-demo.appspot.com/static/media/', 
+	rtl : false, 
+	scrollbars : true, 
+	sounds : true, 
+	oneBasedIndex : true, 
+	grid : {
+		spacing : 100, 
+		length : 5, 
+		colour : '#888', 
+		snap : false
+	}, 
+	zoom : {
+		controls : true, 
+		wheel : true, 
+		startScale : 5, 
+		maxScale : 15, 
+		minScale : 0.75, 
+		scaleSpeed : 1.2
+	}
+};
 
-	var workspace = Blockly.inject('blocklyDiv', options);
+/* Inject your workspace */ 
+var workspace = Blockly.inject('blocklyDiv',options);
 
-	var workspaceBlocks = document.getElementById("workspaceBlocks"); 
-	Blockly.Xml.domToWorkspace(workspaceBlocks, workspace);
-});
+/* Load Workspace Blocks from XML to workspace. Remove all code below if no blocks to load */
+
+// /* TODO: Change workspace blocks XML ID if necessary. Can export workspace blocks XML from Workspace Factory. */
+// var workspaceBlocks = document.getElementById("workspaceBlocks"); 
+
+// /* Load blocks to workspace. */
+// Blockly.Xml.domToWorkspace(workspaceBlocks, workspace);
